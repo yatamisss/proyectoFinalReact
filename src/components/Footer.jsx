afa27f17c847d0a8e026021c11ce97a8fd43960c
yatamisss <yatamispo@gmail.com>
2023-07-23 15:32:13 -0400

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <>
             <footer className="container-footer">
                 <div className="container-footer__informacion">
-                    <a href="https://github.com/yatamisss/proyectoFinalReact">Link a repositorio</a>
+                    <a href="https://github.com/yatamisss/proyectoFinalReact" target="_blank">Link a repositorio</a>
                     <p>No nos contactes por aqui por favor</p>
                     <p>@paginafake</p>
                     <p>©todos los derechos reservados</p>
